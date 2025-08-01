c728912a93742f02e819abff1db39b2719a39326
m4rtinzz <contatotin41@gmail.com>
2025-08-01 11:05:25 -0300

    Formatação Moeda. Tratamento de erro.

--- poo-encapsulamento/src/App.tsx
+++ poo-encapsulamento/src/App.tsx
@@ -1,26 +1,42 @@
 import { useState } from 'react'
 import './App.css'
 import { ContaBancaria } from './models/ContaBancaria';
+import { formatarMoeda } from './utils/formatador';
 
 function App()
 {
   const [valor, setValor] = useState(0);
   const [conta] = useState(new ContaBancaria());
   const [saldo, setSaldo] = useState(conta.verSaldo());
+  const [erro, setErro] = useState('');
 
   const handleDepositar = () => {
+    if (valor <= 0) {
+      setErro('Digite um valor válido para depósito.');
+      return;
+    }
     conta.depositar(valor);
     setSaldo(conta.verSaldo());
+    setErro('');
   };
 
   const handleSacar = () => {
+    if (valor <= 0) {
+      setErro('Digite um valor válido para saque.');
+      return;
+    }
+    if (valor > saldo) {
+      setErro('Saldo insuficiente para saque.');
+      return;
+    }
     conta.sacar(valor);
     setSaldo(conta.verSaldo());
+    setErro('');
   };
 
   return (
     <div>
-      <h3>Saldo disponível: R$ {saldo}</h3>
+      <h3>Saldo disponível: {formatarMoeda(saldo)} </h3>
       <input
         type="number"
         value={valor}
@@ -29,6 +45,8 @@ function App()
       />
       <button onClick={handleDepositar}>Depositar</button>
       <button onClick={handleSacar}>Sacar</button>
+      {erro && <p style={{ color: 'red' }}>{erro}</p>}
+
     </div>
   );
 }
